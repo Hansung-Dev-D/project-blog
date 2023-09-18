6ebcbe661953dbfa1c22d14be162bc7987f71148
chaeyoung <chae00901@naver.com>
2023-09-18 17:07:41 +0900

    로그인 페이지 구현

--- blog/src/pages/Login.js
+++ blog/src/pages/Login.js
@@ -1,7 +1,95 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+
+
+const styles = {
+    universal: {
+        boxSizing: 'border-box',
+    },
+    //전체 div
+    body: {
+        background: '#f6f5f7',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        fontFamily: "'Montserrat', sans-serif",
+        height: '100vh',
+        margin: '-20px 0 50px',
+    },
+    //확인 버튼
+    button: {
+        borderRadius: '20px',
+        border: '1px solid #96d5ff',
+        backgroundColor: '#96d5ff',
+        color: '#FFFFFF',
+        fontSize: '12px',
+        fontWeight: 'bold',
+        padding: '12px 45px',
+        letterSpacing: '1px',
+        textTransform: 'uppercase',
+        transition: 'transform 80ms ease-in',
+        margin: '8px 0 20px 0',
+    },
+    buttonActive: {
+        transform: 'scale(0.95)',
+    },
+    buttonFocus: {
+        outline: 'none',
+    },
+    buttonGhost: {
+        backgroundColor: 'transparent',
+        borderColor: '#FFFFFF',
+    },
+    form: {
+        backgroundColor: '#FFFFFF',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        padding: '0 50px',
+        height: '100%',
+        textAlign: 'center',
+    },
+    //아이디 비번 입력 창
+    input: {
+        backgroundColor: '#eee',
+        border: 'none',
+        padding: '12px 15px',
+        margin: '8px 0 20px 0', // 아래쪽 여백을 20px로 조정
+        width: '100%',
+    },
+    //로그인 폼 테두리
+    container: {
+        backgroundColor: '#fff',
+        borderRadius: '10px',
+        boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
+        position: 'relative',
+        overflow: 'hidden',
+        width: '768px',
+        maxWidth: '100%',
+        minHeight: '480px',
+    },
+    formContainer: {
+        position: 'absolute',
+        top: 0,
+        height: '100%',
+        transition: 'all 0.6s ease-in-out',
+    },
+    overlayContainer: {
+        position: 'absolute',
+        top: 0,
+        left: '50%',
+        width: '50%',
+        height: '100%',
+        overflow: 'hidden',
+        transition: 'transform 0.6s ease-in-out',
+        zIndex: 100,
+    },
+};
+
 
 function Login() {
     const [inputId, setInputId] = useState('');
@@ -56,10 +144,11 @@ function Login() {
         alert('비밀번호를 찾습니다.');
     };
 
+
     return (
-        <div>
-            <div className="login wrap">
-                <h1 style={{ marginBottom: '40px', fontSize: '35px' }}>로그인</h1>
+        <div style={styles.container}>
+            <div className="login wrap" >
+                <h1 style={{ marginBottom: '40px', fontSize: '35px' }}>LOGIN</h1>
                 <Form onSubmit={handleSubmit}>
                     <div className="id_pw_align">
                         <Form.Label>아이디</Form.Label>
@@ -68,8 +157,7 @@ function Login() {
                             name="input_id"
                             value={inputId}
                             onChange={handleInputId}
-                            style={{ height: '45px' }}
-                        />
+                            style={styles.input} />
                         <Form.Label htmlFor="inputPassword5">비밀번호</Form.Label>
                         <Form.Control
                             type="password"
@@ -78,10 +166,10 @@ function Login() {
                             name="input_pw"
                             value={inputPw}
                             onChange={handleInputPw}
-                            style={{ height: '45px' }}
+                            style={styles.input}
                         />
                     </div>
-                    <Button variant="outline-success" size="large" style={{marginTop: '13%', width: "180px"}} type="submit">
+                    <Button variant="outline-success" size="large" style={styles.button} type="submit">
                         확인
                     </Button>
                 </Form>
@@ -95,7 +183,7 @@ function Login() {
           </span>
                 </div>
                 <div style={{ marginTop: '13%' }}>
-                    <Button variant="outline-success" size="large" onClick={onClickSignup}>
+                    <Button variant="outline-success" size="large" style={styles.button} onClick={onClickSignup}>
                         회원가입
                     </Button>
                 </div>
